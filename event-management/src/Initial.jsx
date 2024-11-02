5515449bc37c0bd5ef515e767acb35a3983c5422
Kyle Uy <kyleeeee09@gmail.com>
2024-11-02 19:40:50 +0800

    re commit main

--- event-management/src/Initial.jsx
+++ event-management/src/Initial.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { supabase } from "../API/createClient";
-import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
+
 import Form from "./components/form";
 
 const App = () => {
@@ -19,9 +19,9 @@ const App = () => {
 
   async function deleteGuest(id) {
     const { data, error } = await supabase.from("guests").delete().eq("id", id);
-    
-    if(error){
-        console.log("error", error);
+
+    if (error) {
+      console.log("error", error);
     }
 
     fetchGuests();
@@ -31,14 +31,16 @@ const App = () => {
     <>
       <div>
         <h1>Guest List</h1>
-        <table class='table table-hover '>
+        <table class="table table-hover ">
           <thead>
             <tr>
-              <th scope='col'>#</th>
-              <th scope='col'>Name</th>
-              <th scope='col'>Guest</th>
-              <th scope='col'>Comapny Name</th>
-              <th scope='col' colSpan={2}>Actions</th>
+              <th scope="col">#</th>
+              <th scope="col">Name</th>
+              <th scope="col">Guest</th>
+              <th scope="col">Comapny Name</th>
+              <th scope="col" colSpan={2}>
+                Actions
+              </th>
             </tr>
           </thead>
           <tbody>
@@ -49,12 +51,17 @@ const App = () => {
                 <td>{guest.guest}</td>
                 <td>{guest.company_name}</td>
                 <td>
-                  <button class='btn btn-success'  onClick={<Form/>}>
+                  <button class="btn btn-success" onClick={<Form />}>
                     Edit
                   </button>
                 </td>
                 <td>
-                  <button class='btn btn-danger' onClick={() => {deleteGuest(guest.id)}}>
+                  <button
+                    class="btn btn-danger"
+                    onClick={() => {
+                      deleteGuest(guest.id);
+                    }}
+                  >
                     Delete
                   </button>
                 </td>
@@ -65,42 +72,42 @@ const App = () => {
       </div>
 
       <button
-        type='button'
-        class='btn btn-primary'
-        data-bs-toggle='modal'
-        data-bs-target='#exampleModal'
+        type="button"
+        class="btn btn-primary"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
       >
         Register
       </button>
 
       <div
-        class='modal fade'
-        id='exampleModal'
-        tabindex='-1'
-        aria-labelledby='exampleModalLabel'
-        aria-hidden='true'
+        class="modal fade"
+        id="exampleModal"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
       >
-        <div class='modal-dialog'>
-          <div class='modal-content'>
-            <div class='modal-header'>
-              <h1 class='modal-title fs-5' id='exampleModalLabel'>
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">
                 Register Guest
               </h1>
               <button
-                type='button'
-                class='btn-close'
-                data-bs-dismiss='modal'
-                aria-label='Close'
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
               ></button>
             </div>
-            <div class='modal-body'>
+            <div class="modal-body">
               <Form />
             </div>
-            <div class='modal-footer'>
+            <div class="modal-footer">
               <button
-                type='button'
-                class='btn btn-secondary'
-                data-bs-dismiss='modal'
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
               >
                 Close
               </button>
