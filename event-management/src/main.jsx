110830fb1c188320f8e2c7d7d3c635ccfea6344c
Fergus Miles Ampuan <fergus.ampuan@gmail.com>
2024-11-02 20:56:40 +0800

    WalkIn is now working, changed main from initial  to  app

--- event-management/src/main.jsx
+++ event-management/src/main.jsx
@@ -4,14 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import Initial from './initial.jsx'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Initial />
+    <App />
   </StrictMode>,
 )
