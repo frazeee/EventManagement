86accc9ae26814e19965a0f58e854d43df483e1c
Kyle Uy <kyleeeee09@gmail.com>
2024-11-02 21:52:36 +0800

    Update walkIn

--- event-management/src/components/walkIn/walkIn.jsx
+++ event-management/src/components/walkIn/walkIn.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { supabase } from "../../API/createClient";
+import { supabase } from "../../../API/createClient";
 
 const WalkIn = () => {
   const handleChange = (e) => {
@@ -16,104 +16,119 @@ const WalkIn = () => {
     guest: "",
     company_name: "",
   });
+  const [showModal, setShowModal] = useState(false);
 
-  async function createGuest() {
-    await supabase.from("guests").insert({
-      name: guest.name,
-      guest: guest.guest,
-      company_name: guest.company_name,
-      registration_type: "WALK-IN",
-    });
-  }
+  const createGuest = async (e) => {
+    e.preventDefault();
+    try {
+      const { data, error } = await supabase
+        .from("guests")
+        .insert({
+          name: guest.name,
+          guest: guest.guest,
+          company_name: guest.company_name,
+          registration_type: "WALK-IN",
+        })
+        .select();
+      if (error) throw error;
+      else {
+        setShowModal(false);
+      }
+    } catch (error) {
+      console.error("An unexpected error occurred:", error);
+    }
+  };
+
   return (
     <div>
       <button
-        type='button'
-        className='btn btn-primary mx-2'
-        data-bs-toggle='modal'
-        data-bs-target='#staticBackdrop2'
+        type="button"
+        className="btn btn-primary mx-2"
+        onClick={() => setShowModal(true)}
       >
         Walk-In
       </button>
 
-      <form></form>
-      <div
-        className='modal fade'
-        id='staticBackdrop2'
-        data-bs-backdrop='static'
-        data-bs-keyboard='false'
-        tabindex='-1'
-        aria-labelledby='staticBackdropLabel'
-        aria-hidden='true'
-      >
-        <div className='modal-dialog modal-dialog-centered'>
-          <div className='modal-content'>
-            <div className='modal-header'>
-              <h1 className='modal-title fs-5' id='staticBackdropLabel'>
-                Register Walk-In Guest
-              </h1>
-              <button
-                type='button'
-                className='btn-close'
-                data-bs-dismiss='modal'
-                aria-label='Close'
-              ></button>
-            </div>
-            <div className='modal-body'>
-              <form onSubmit={createGuest}>
-                <div class='mb-3'>
-                  <label for='name' class='form-label'>
-                    Full Name
-                  </label>
-                  <input
-                    type='text'
-                    class='form-control'
-                    id='name'
-                    name='name'
-                    aria-describedby='name'
-                    onChange={handleChange}
-                    value={guest.name}
-                  />
-                </div>
-                <div class='mb-3'>
-                  <label for='guest' class='form-label'>
-                    Guest
-                  </label>
-                  <input
-                    type='text'
-                    class='form-control'
-                    id='guest'
-                    name='guest'
-                    aria-describedby='guest'
-                    onChange={handleChange}
-                    value={guest.guest}
-                  />
-                </div>
-                <div class='mb-3'>
-                  <label for='company_name' class='form-label'>
-                    Company Name
-                  </label>
-                  <input
-                    type='text'
-                    class='form-control'
-                    id='company_name'
-                    name='company_name'
-                    aria-describedby='company_name'
-                    onChange={handleChange}
-                    value={guest.company_name}
-                  />
-                </div>
+      {showModal && (
+        <div
+          className="modal fade show"
+          id="staticBackdrop2"
+          style={{ display: 'block' }}
+          data-bs-backdrop="static"
+          data-bs-keyboard="false"
+          aria-labelledby="staticBackdropLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog modal-dialog-centered">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="staticBackdropLabel">
+                  Register Walk-In Guest
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  onClick={() => setShowModal(false)}
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <form onSubmit={createGuest}>
+                  <div className="mb-3">
+                    <label htmlFor="name" className="form-label">
+                      Full Name
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="name"
+                      name="name"
+                      aria-describedby="name"
+                      onChange={handleChange}
+                      value={guest.name}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="guest" className="form-label">
+                      Guest
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="guest"
+                      name="guest"
+                      aria-describedby="guest"
+                      onChange={handleChange}
+                      value={guest.guest}
+                    />
+                  </div>
+                  <div className="mb-3">
+                    <label htmlFor="company_name" className="form-label">
+                      Company Name
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="company_name"
+                      name="company_name"
+                      aria-describedby="company_name"
+                      onChange={handleChange}
+                      value={guest.company_name}
+                    />
+                  </div>
 
-                <button type='submit' class='btn btn-primary'>
-                  Submit
-                </button>
-              </form>
+                  <button type="submit" className="btn btn-primary">
+                    Submit
+                  </button>
+                </form>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
 
 export default WalkIn;
+
