ccf30e039102ff1812fe8f6f7a162f107b50cd49
Fergus Miles Ampuan <fergus.ampuan@gmail.com>
2024-11-02 19:30:33 +0800

    CRUD is working except edit

--- event-management/API/createClient.js
+++ event-management/API/createClient.js
@@ -1,7 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-//  console.log(process.env.VITE_SUPABASE_KEY);
-console.log(import.meta.env.VITE_SUPABASE_KEY);
 
 const supabaseUrl = 'https://anzspqpmocdptqesqzzs.supabase.co';
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
--- event-management/src/Initial.jsx
+++ event-management/src/Initial.jsx
@@ -1,28 +1,115 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { supabase } from "../API/createClient";
+import { BrowserRouter as Router, Route, Routes, useNavigate } from "react-router-dom";
+import Form from "./components/form";
 
 const App = () => {
+  const [guests, setGuests] = useState([]);
 
-    const [guests, setGuests] = useState([]);
-    console.log(guests);
+  useEffect(() => {
+    fetchGuests();
+  }, []);
 
-    useEffect(() => {
-        fetchGuests();
-    }, []);
+  async function fetchGuests() {
+    const { data } = await supabase.from("guests").select("*");
+    // console.log("data", data);
 
-    async function fetchGuests(){
-        const {data} = await supabase.from('guests').select("*")
-        console.log("data",data);
+    setGuests(data);
+  }
 
-        setGuests(data);
-        
+  async function deleteGuest(id) {
+    const { data, error } = await supabase.from("guests").delete().eq("id", id);
+    
+    if(error){
+        console.log("error", error);
     }
 
-    return (
-        <div>
-        <h1>Event Management</h1>
+    fetchGuests();
+  }
+
+  return (
+    <>
+      <div>
+        <h1>Guest List</h1>
+        <table class='table table-hover '>
+          <thead>
+            <tr>
+              <th scope='col'>#</th>
+              <th scope='col'>Name</th>
+              <th scope='col'>Guest</th>
+              <th scope='col'>Comapny Name</th>
+              <th scope='col' colSpan={2}>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {guests.map((guest, index) => (
+              <tr key={guest.id}>
+                <td>{index + 1}</td>
+                <td>{guest.name}</td>
+                <td>{guest.guest}</td>
+                <td>{guest.company_name}</td>
+                <td>
+                  <button class='btn btn-success'  onClick={<Form/>}>
+                    Edit
+                  </button>
+                </td>
+                <td>
+                  <button class='btn btn-danger' onClick={() => {deleteGuest(guest.id)}}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      <button
+        type='button'
+        class='btn btn-primary'
+        data-bs-toggle='modal'
+        data-bs-target='#exampleModal'
+      >
+        Register
+      </button>
+
+      <div
+        class='modal fade'
+        id='exampleModal'
+        tabindex='-1'
+        aria-labelledby='exampleModalLabel'
+        aria-hidden='true'
+      >
+        <div class='modal-dialog'>
+          <div class='modal-content'>
+            <div class='modal-header'>
+              <h1 class='modal-title fs-5' id='exampleModalLabel'>
+                Register Guest
+              </h1>
+              <button
+                type='button'
+                class='btn-close'
+                data-bs-dismiss='modal'
+                aria-label='Close'
+              ></button>
+            </div>
+            <div class='modal-body'>
+              <Form />
+            </div>
+            <div class='modal-footer'>
+              <button
+                type='button'
+                class='btn btn-secondary'
+                data-bs-dismiss='modal'
+              >
+                Close
+              </button>
+            </div>
+          </div>
         </div>
-    );
-}
+      </div>
+    </>
+  );
+};
 
 export default App;
