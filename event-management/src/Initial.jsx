d3aa2dd843388ae18cf6d8b50e3bb0de3c23903e
Fergus Miles Ampuan <fergus.ampuan@gmail.com>
2024-11-03 14:02:20 +0800

    CRUD now working

--- event-management/src/Initial.jsx
+++ event-management/src/Initial.jsx
@@ -5,15 +5,14 @@ import Form from "./components/form";
 
 const App = () => {
   const [guests, setGuests] = useState([]);
+  const [editingGuest, setEditingGuest] = useState(null);
 
   useEffect(() => {
     fetchGuests();
   }, []);
 
   async function fetchGuests() {
-    const { data } = await supabase.from("guests").select("*");
-    // console.log("data", data);
-
+    const { data } = await supabase.from("guests").select("*").order('id', { ascending: true });
     setGuests(data);
   }
 
@@ -29,15 +28,15 @@ const App = () => {
 
   return (
     <>
-      <div>
-        <h1>Guest List</h1>
-        <table class="table table-hover ">
+      <div className="container-md">
+        <h1 className="text-center py-5">Guest List</h1>
+        <table className="table table-hover">
           <thead>
-            <tr>
-              <th scope="col">#</th>
+            <tr className="text-center">
+              <th scope="col"># </th>
               <th scope="col">Name</th>
               <th scope="col">Guest</th>
-              <th scope="col">Comapny Name</th>
+              <th scope="col">Company Name</th>
               <th scope="col" colSpan={2}>
                 Actions
               </th>
@@ -45,22 +44,25 @@ const App = () => {
           </thead>
           <tbody>
             {guests.map((guest, index) => (
-              <tr key={guest.id}>
+              <tr className="text-center" key={guest.id}>
                 <td>{index + 1}</td>
                 <td>{guest.name}</td>
                 <td>{guest.guest}</td>
                 <td>{guest.company_name}</td>
                 <td>
-                  <button class="btn btn-success" onClick={<Form />}>
+                  <button
+                    className="btn btn-success"
+                    data-bs-toggle="modal"
+                    data-bs-target="#editModal"
+                    onClick={() => setEditingGuest(guest)}
+                  >
                     Edit
                   </button>
                 </td>
                 <td>
                   <button
-                    class="btn btn-danger"
-                    onClick={() => {
-                      deleteGuest(guest.id);
-                    }}
+                    className="btn btn-danger"
+                    onClick={() => deleteGuest(guest.id)}
                   >
                     Delete
                   </button>
@@ -69,48 +71,86 @@ const App = () => {
             ))}
           </tbody>
         </table>
-      </div>
 
-      <button
-        type="button"
-        class="btn btn-primary"
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-      >
-        Register
-      </button>
+        <button
+          type="button"
+          className="btn btn-primary"
+          data-bs-toggle="modal"
+          data-bs-target="#exampleModal"
+        >
+          Register
+        </button>
 
-      <div
-        class="modal fade"
-        id="exampleModal"
-        tabindex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">
-                Register Guest
-              </h1>
-              <button
-                type="button"
-                class="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
+        <div
+          className="modal fade"
+          id="exampleModal"
+          tabIndex="-1"
+          aria-labelledby="exampleModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">
+                  Register Guest
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                ></button>
+              </div>
+              <div className="modal-body">
+                <Form />
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                >
+                  Close
+                </button>
+              </div>
             </div>
-            <div class="modal-body">
-              <Form />
-            </div>
-            <div class="modal-footer">
-              <button
-                type="button"
-                class="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
-                Close
-              </button>
+          </div>
+        </div>
+
+        <div
+          className="modal fade"
+          id="editModal"
+          tabIndex="-1"
+          aria-labelledby="editModalLabel"
+          aria-hidden="true"
+        >
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="editModalLabel">
+                  Edit Guest
+                </h1>
+                <button
+                  type="button"
+                  className="btn-close"
+                  data-bs-dismiss="modal"
+                  aria-label="Close"
+                  onClick={() => setEditingGuest(null)}
+                ></button>
+              </div>
+              <div className="modal-body">
+                <Form guest={editingGuest} />
+              </div>
+              <div className="modal-footer">
+                <button
+                  type="button"
+                  className="btn btn-secondary"
+                  data-bs-dismiss="modal"
+                  onClick={() => setEditingGuest(null)}
+                >
+                  Close
+                </button>
+              </div>
             </div>
           </div>
         </div>
